b64c27bd053086ca72c8dc594317b4cb2a010686
= <vnitusingh@yahoo.com>
2020-10-29 18:16:53 -0400

    code for linear regression to predict mpg

---
+++ MechaCarChallenge.R
@@ -0,0 +1,5 @@
+library(dbplyr)
+MechaCar <- read.csv(file = 'MechaCar_mpg.csv', check.names = F, stringsAsFactors = F)
+
+lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=MechaCar) #generate multiple linear regression model
+summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=MechaCar)) #generate summary statistics

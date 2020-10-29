034820c74dbbb2f04113312929776de7038a75b8
= <vnitusingh@yahoo.com>
2020-10-29 19:07:31 -0400

    total and lot summary statistics on Suspension Coil

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -3,3 +3,8 @@ MechaCar <- read.csv(file = 'MechaCar_mpg.csv', check.names = F, stringsAsFactor
 
 lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=MechaCar) #generate multiple linear regression model
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=MechaCar)) #generate summary statistics
+
+
+SusCoil <- read.csv(file = 'Suspension_Coil.csv', check.names = F, stringsAsFactors = F)
+total_summary <- SusCoil %>% summarize(mean(PSI),median(PSI),var(PSI), sd(PSI), .groups = 'keep')
+lot_summary <- SusCoil %>% group_by(Manufacturing_Lot) %>% summarize(mean(PSI),median(PSI),var(PSI), sd(PSI), .groups= 'keep')
